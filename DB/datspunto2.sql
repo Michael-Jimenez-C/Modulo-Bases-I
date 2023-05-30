1ac8056982f288407aa5e8c06ce4271d4437e9f9
Michael-Steven-Jimenez-contreras <msjimenezc@correo.udistrital.edu.co>
2023-05-30 12:34:24 -0500

    termine las insersiones.

--- DB/datspunto2.sql
+++ DB/datspunto2.sql
@@ -8,11 +8,24 @@ ID DESCTIPOCALENDARIO
 3  Seleccion
 4  Ensayo
 5  Funcion
+
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ IDTIPOCALEN                               NOT NULL VARCHAR2(2)
+ IDOBRA                                    NOT NULL VARCHAR2(4)
+ CONSECCALENDARIO                          NOT NULL NUMBER(4)
+ IDESTADO                                  NOT NULL VARCHAR2(10)
+ FECHAINICIO                               NOT NULL TIMESTAMP(6)
+ FECHAFIN                                  NOT NULL TIMESTAMP(6)
 */
 
-insert into calendario values (1,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-02-01 13:00:00',timestamp '2023-06-10 13:00:00');
-insert into calendario values (2,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-02-01 13:00:00',timestamp '2023-06-10 13:00:00');
-insert into calendario values (3,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-02-01 13:00:00',timestamp '2023-06-10 13:00:00');
+CREATE UNIQUE INDEX idx_actividades_obra
+ON calendario (idobra, fechaInicio, fechaFin);
+
+
+insert into calendario values (1,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-02-01 13:00:00',timestamp '2023-03-10 13:00:00');
+insert into calendario values (2,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-03-01 13:00:00',timestamp '2023-04-10 13:00:00');
+insert into calendario values (3,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-04-01 13:00:00',timestamp '2023-05-10 13:00:00');
 
 
 insert into calendario values (4,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-02-01 14:00:00',timestamp '2023-02-01 16:00:00');
@@ -36,3 +49,10 @@ insert into calendario values (5,2,(select coalesce(v,0)+1 from (select max(cons
 insert into calendario values (5,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-03-31 14:00:00',timestamp '2023-03-31 16:00:00');
 insert into calendario values (5,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-03-31 14:00:00',timestamp '2023-03-31 16:00:00');
 
+insert into tipocalendario values ('6','Reporte de estudiantes');
+insert into tipocalendario values ('7','Liquidacion de empleados');
+insert into tipocalendario values ('8','Liquidacion gastos');
+
+insert into calendario values (6,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-04-1 10:00:00',timestamp '2023-04-1 22:00:00');
+insert into calendario values (7,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-04-2 10:00:00',timestamp '2023-04-2 22:00:00');
+insert into calendario values (8,2,(select coalesce(v,0)+1 from (select max(conseccalendario) v from calendario)),'Activo',timestamp '2023-04-3 10:00:00',timestamp '2023-04-3 22:00:00');
